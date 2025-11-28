2bf05fdc1116f55b8821c561ddb174e62c29be21
Ahmad Meda <107063050+ahmad-meda@users.noreply.github.com>
2025-11-28 16:02:48 +0400

    Add methods to manage comments on mood records

--- proxies/employee_mood_proxy.py
+++ proxies/employee_mood_proxy.py
@@ -1,4 +1,5 @@
 from services.employee_mood_service import EmployeeMoodService
+from Files.SQLAlchemyModels import MoodCheck
 
 class EmployeeMoodProxy:
     @staticmethod
@@ -6,3 +7,17 @@ class EmployeeMoodProxy:
         from app import app, db
         with app.app_context():
             return EmployeeMoodService.get_mood_check_statistics(db.session, company_ids, group_id, start_date, end_date)
+    
+    @staticmethod
+    def add_comment_to_mood_record(mood_record: MoodCheck, comment: str):
+        """Add or update a comment to a mood check record."""
+        from app import app, db
+        with app.app_context():
+            return EmployeeMoodService.add_comment_to_mood_record(db.session, mood_record, comment)
+    
+    @staticmethod
+    def add_comment_to_mood_record_by_id(mood_id: int, comment: str):
+        """Add or update a comment to a mood check record by ID."""
+        from app import app, db
+        with app.app_context():
+            return EmployeeMoodService.add_comment_to_mood_record_by_id(db.session, mood_id, comment)
